f264cef374315502f868cde76121da141c68b3db
bdashore3 <bdashore3@gmail.com>
2020-01-16 18:30:32 -0500

    Botwide: Rewrite CRUD Operations
    Thanks tanish2k09 for helping me with the new stringify function
    which works with arrays
    
    Rewrite the usage of the CRUD system to create a readable json format
    for peoples' balances instead of having names strewn about
    
    Also, fix the critical error where all users would get the balance
    of a single user when executing ^bal. Users can now get their own
    balances.
    
    TODO: If a user's balance is undefined, ask him or her to create a
    new account.
    
    Signed-off-by: bdashore3 <bdashore3@gmail.com>

--- econ.js
+++ econ.js
@@ -8,11 +8,10 @@ const jsonPath = "./JSON/";
 const selfID = ids.self;
 const quantumID = ids.quantum;
 const briID = ids.bri;
-const retracksID = ids.retracks;
-const bebeID = ids.bebe;
 const prefix = '^';
 const client = new Discord.Client();
 const alphaNum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+var isBreak = false;
 
 function isAdmin(userID) {
 	return (isDev(userID));
@@ -56,17 +55,34 @@ client.on('message', async message => {
     switch (command) {
         case "addmoney":
             if (isAdmin(message.author.id)) {
-                var bal_to_add = words[1]
-                balance.add(message.author.id, bal_to_add)
-                message.reply(` added ` + bal_to_add + ` to the account`)
+                var addBalance = Number(words[1])
+                balance.add(message.author.id, addBalance)
+                message.reply(` added ` + addBalance + ` to the account`)
             }
             else {
-                message.channel.send(`Sorry, only bot admins can do this`)
+				message.channel.send(`Sorry, only bot admins can do this`)
 			}
+			break;
 		
-		case "showbal":
-			balanceList = JSON.parse(fs.readFileSync(jsonPath + 'balance.json')); 
-				message.reply(`you have ` + balanceList.balance + `.`)
+		case "showbal": 
+		case "bal": 
+			balance.updateBalList()
+			reply = balance.getCurBalance(message.author.id)
+			message.reply(`you have ` + reply + `.`)
+			break;
+		
+		case "daily":
+			var timeout = 86400000 // 24 hours in milliseconds, change if you'd like.
+			var amount = 100
+			balance.updateMoney(message.author.id, amount)
+			message.reply(` added ` + amount + ` to the account`)
+			break;
+		
+		case "delete":
+			var amount = Number(words[1])
+			balance.rmMoney(message.author.id, amount)
+			message.reply (` removed ` + amount + ` from the account`)
+			break;
 		
 			
     }
--- helpers/balance.js
+++ helpers/balance.js
@@ -5,20 +5,71 @@
  *
  */
 
- const fs = require('fs');
+const fs = require('fs');
 
- const jsonPath = "./JSON/";
+const jsonPath = "./JSON/";
+
+var people = {};
+
+function stringify() {
+    out = "{\n\t\"people\" : [";
+    isInit = true;
+    for (name in people) {
+        if (!isInit)
+            out += ",";
+
+        out += "\n\t\t{\n\t\t\t\"name\" : \"" + name + "\",";
+        out += "\n\t\t\t\"balance\" : \"" + people[name] + "\"\n\t\t}"
+        isInit = false;
+    }
+    out += "\n\t]\n}";
+    return out;
+}
 
 module.exports = {
-    add: function(username, newbalance) {
-        let profile = {
-            name: username,
-            balance: newbalance
+    updateBalList: function() {
+        balanceList = JSON.parse(fs.readFileSync(jsonPath + 'balance.json'));
+		for (var i = 0; i < balanceList.people.length; ++i) {
+			curBalance = balanceList.people[i];
+            people[curBalance.name] = curBalance.balance;
         }
-        let data = JSON.stringify(profile, null, 2)
-        fs.writeFileSync(jsonPath + 'balance.json', data);
+    },
+    
+    getCurBalance: function (userID) {
+        sortedList = [];
+
+        for (name in people) {
+            sortedList[sortedList.length] = name;
+        }
+
+        sortedList.sort();
+
+        userIndex = sortedList.indexOf(userID);
+        username = sortedList[userIndex];
+
+        out = people[sortedList[userIndex]];
+        return out;
+    
+    },
+
+    add: function(userID, newbalance) {
+        
+        people[userID] = newbalance;
+        fs.writeFileSync(jsonPath + 'balance.json', stringify());
         return "Balance successfully written";
+    },
+
+    updateMoney: function(userID, amount) {
+        this.getCurBalance(userID)
+        newbalance = Number(out) + amount
+        this.add(userID, newbalance)
+        return "Successfully updated balance";
+    },
+
+    rmMoney: function(userID, amount) {
+        this.getCurBalance(userID)
+        newbalance = Number(out) - amount
+        this.add(userID, newbalance)
+        return "Successfully updated balance";
     }
-
-
 }
